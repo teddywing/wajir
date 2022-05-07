21bef67ff67f67b8bd2e6a351d75e4e9cf94e786
Teddy Wing <git@teddywing.com>
2022-05-07 02:21:19 +0200

    Move issues fetch to a function

--- src/main.lisp
+++ src/main.lisp
@@ -7,11 +7,21 @@
   ;; Send email to ^maildir^program^ containing message with issue metadata
   ;; Continue to next page
 
-  (dex:post "https://example.atlassian.net/rest/api/3/search"
-            :content
-            "{
-            \"jql\": \"project = \\\"FAKE\\\" AND watcher != currentUser() AND key > \\\"FAKE-100\\\" ORDER BY created DESC\",
-            \"fields\": [\"id\", \"key\", \"self\"]
-            }"
-            :headers '((:content-type . "application/json")
-                       (:authorization . "Basic TOKEN"))))
+  (fetch-issues
+    "project = \\\"FAKE\\\" AND watcher != currentUser() AND key > \\\"FAKE-100\\\" ORDER BY created DESC"
+    :basic-auth-token "TOKEN"))
+
+(defun fetch-issues (jql &key basic-auth-token)
+  (multiple-value-bind (body code)
+    (dex:post "https://example.atlassian.net/rest/api/3/search"
+              :content
+              (format nil "{
+                      \"jql\": \"~A\",
+                      \"fields\": [\"id\", \"key\", \"self\"]
+                      }"
+                      jql)
+              :headers `((:content-type . "application/json")
+                         (:authorization
+                          . ,(format nil "Basic ~A" basic-auth-token))))
+
+    body))
