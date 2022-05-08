8d98dd58afdbf659b6c246f158451cd8c850acb8
Teddy Wing <git@teddywing.com>
2022-05-08 04:06:28 +0200

    config.lisp: Fix formatting

--- src/config.lisp
+++ src/config.lisp
@@ -2,18 +2,18 @@
 
 (defclass config ()
   ((login
-    :initarg :login
-    :reader login
-    :documentation "Jira login email address")
-  (token
-    :initarg :token
-    :reader token
-    :documentation "Jira authentication token")
-  (endpoint
-    :initarg :endpoint
-    :reader endpoint
-    :documentation "Jira site URL (e.g. example.atlassian.net)")
-  (jql
-    :initarg :jql
-    :reader jql
-    :documentation "JQL querying issues to watch")))
+     :initarg :login
+     :reader login
+     :documentation "Jira login email address")
+   (token
+     :initarg :token
+     :reader token
+     :documentation "Jira authentication token")
+   (endpoint
+     :initarg :endpoint
+     :reader endpoint
+     :documentation "Jira site URL (e.g. example.atlassian.net)")
+   (jql
+     :initarg :jql
+     :reader jql
+     :documentation "JQL querying issues to watch")))
